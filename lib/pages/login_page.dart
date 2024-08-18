c97139ead16602f0dc976d9c05f072717a31672c
BlackHatDevX <jashgro@yandex.com>
2024-08-19 00:30:26 +0530

    animated Container and stateful widget

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,9 +3,16 @@
 import "package:flutter/material.dart";
 import "package:flutter_catalog/utils/routes.dart";
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,7 +25,7 @@ class LoginPage extends StatelessWidget {
               height: 20,
             ),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
@@ -34,6 +41,10 @@ class LoginPage extends StatelessWidget {
                   TextFormField(
                     decoration: InputDecoration(
                         hintText: "Enter username", labelText: "Username"),
+                    onChanged: (val) {
+                      name = val;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     decoration: InputDecoration(
@@ -43,16 +54,52 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 20,
                   ),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                      style: TextButton.styleFrom(
-                        minimumSize: Size(300, 40),
-                        backgroundColor: Colors.indigoAccent,
-                        foregroundColor: Colors.white,
-                      ),
-                      child: Text("Login"))
+                  InkWell(
+                    splashColor: Colors.red,
+                    onTap: () async {
+                      setState(() {
+                        if (changeButton) {
+                          changeButton = false;
+                        } else {
+                          changeButton = true;
+                        }
+                      });
+                      await Future.delayed(Duration(milliseconds: 1000));
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 1000),
+                      height: 50,
+                      width: changeButton ? 50 : 150,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius:
+                              BorderRadius.circular(changeButton ? 50 : 8)),
+                      child: changeButton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
+                    ),
+                  )
+                  // ElevatedButton(
+                  //     onPressed: () {
+                  //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //     },
+                  //     style: TextButton.styleFrom(
+                  //       minimumSize: Size(300, 40),
+                  //       backgroundColor: Colors.indigoAccent,
+                  //       foregroundColor: Colors.white,
+                  //     ),
+                  //     child: Text("Login"))
                 ],
               ),
             )
